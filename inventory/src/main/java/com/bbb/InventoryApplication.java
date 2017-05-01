506b333d7c955a361fa9d5a9f674d7f1299b32c4
holuser <holuser@docker.oracleworld.com>
2017-05-02 03:57:46 +0530

    Added eurekaServer and pointed inventory & configserver to Eureka

--- inventory/src/main/java/com/bbb/InventoryApplication.java
+++ inventory/src/main/java/com/bbb/InventoryApplication.java
@@ -2,6 +2,7 @@ package com.bbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -14,6 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableDiscoveryClient
 public class InventoryApplication{
 
 	public static void main(String[] args) {
